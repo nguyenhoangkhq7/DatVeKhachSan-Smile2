f22c1123c6addb7c011b4d6260f9e6d27d9c3f24
dongcao0307 <caothanhdong.41118@gmail.com>
2025-01-18 21:38:19 +0700

    add main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,8 +8,15 @@
      */
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("hehe");
+    public static void main(String[] args)
+    {
+        EntityManager em = Persistence.createEntityManagerFactory("mariadb-pu")
+                .createEntityManager();
     }
 }
