9236f93dff60ade070c4c49033813536cb282824
dongcao0307 <caothanhdong.41118@gmail.com>
2025-01-19 20:40:39 +0700

    update Employee

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -3,7 +3,8 @@ package model;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.Date;
+import java.time.LocalDate;
+
 
 @Entity
 @Getter
@@ -17,29 +18,30 @@ public class Employee {
 
     @Id
     @EqualsAndHashCode.Include
-    @Column(name = "maNhanVien", length = 50, nullable = false)
+    @Column(name = "MaNhanVien", length = 50, nullable = false)
     private String maNhanVien;
 
-    @Column(name = "hoTen", length = 100, nullable = false)
+    @Column(name = "HoTen", length = 100, nullable = false)
     private String hoTen;
 
-    @Column(name = "chucVu")
+    @Column(name = "ChucVu")
     private int chucVu;
 
     @Column(name = "SDT", length = 15, nullable = false)
     private String SDT;
 
-    @Column(name = "diaChi", length = 255)
+    @Column(name = "DiaChi", length = 255)
     private String diaChi;
 
-    @Column(name = "email", length = 100)
+    @Column(name = "Email", length = 100)
     private String email;
 
-    @Column(name = "ngaySinh")
-    private Date ngaySinh;
+    @Column(name = "NgaySinh")
+    private LocalDate ngaySinh;
 
     // Thiết lập quan hệ 1-1
-    @OneToOne(mappedBy = "nhanVien", cascade = CascadeType.ALL)
+    @OneToOne
+    @JoinColumn(name = "tenDN", nullable = false)
     @ToString.Exclude
     private Account taiKhoan;
 }
